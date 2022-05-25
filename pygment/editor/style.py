f22923229611e32728ef90cb96641e4503fd25db
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2022-05-25 16:44:02 +0200

    add Style class definition

--- pygment/editor/style.py
+++ pygment/editor/style.py
@@ -1,2 +1,17 @@
-class Style(dict):
-    pass
+from typing import Any, TypeVar
+
+
+_VT = TypeVar("_VT", bound=Any)
+class Style(dict[str, Any]):
+    """ Dictionary based class for defining component visual style. """
+    def get(self, key: str, /, default: _VT) -> _VT:
+        return super().get(key, default)
+    
+    
+    def __getattr__(self, name: str) -> Any:
+        return super().__getitem__(name)
+    
+    
+    def __setattr__(self, name: str, value: Any) -> None:
+        return super().__setitem__(name, value)
+    
