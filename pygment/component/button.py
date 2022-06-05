8f4b4e79264db47647c664c95f590549b3aecbfe
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2022-06-05 19:30:31 +0200

    update default button style

--- pygment/component/button.py
+++ pygment/component/button.py
@@ -9,9 +9,9 @@ class Button(LayoutNode):
     def render(self, surface: pygame.surface.Surface) -> None:
         if not self.style.get("hidden", False, expected_type=bool):
             rect = self.client_rect(surface)
-            color = self.style.get("color", (0,0,0,0), expected_type=_ColorValue)
+            color = self.style.get("color", (255,255,255), expected_type=_ColorValue)
             
-            border_radius = round(max(self.style.get("border_radius", 0, expected_type=int | float), 0))
+            border_radius = round(max(self.style.get("border_radius", 10, expected_type=int | float), 0))
             border_thickness = round(max(self.style.get("border_thickness", 0, expected_type=int | float), 0))
             
             pygame.draw.rect(surface, color, rect, border_radius=border_radius)
